bac74ae884ac5c61efd78424137de67bdcd099dd
CodeRookie84 <144855285+CodeRookie84@users.noreply.github.com>
2025-07-25 02:04:43 +0530

    server.js

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const io = new Server(server, {
   cors: {
     origin: "https://walkie-talkie-pwa.netlify.app",
     methods: ["GET", "POST"]
-  }
+  },
+  // NEW: Add longer timeouts to prevent disconnection during mic activation
+  pingTimeout: 60000, // The server will wait 60 seconds for a pong response
+  pingInterval: 25000 // The server will send a ping every 25 seconds
 });
 
 const PORT = process.env.PORT || 3001;
@@ -24,19 +27,17 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log(`A user connected: ${socket.id}`);
 
-  // A client subscribes to a channel
   socket.on('join-channel', (channelId) => {
     socket.join(channelId);
     console.log(`User ${socket.id} JOINED channel: ${channelId}`);
   });
   
-  // NEW: A client unsubscribes from a channel
   socket.on('leave-channel', (channelId) => {
     socket.leave(channelId);
     console.log(`User ${socket.id} LEFT channel: ${channelId}`);
   });
 
-  // This part remains the same
+  // We are using the corrected version with two arguments
   socket.on('audio-message', (channel, audioChunk) => {
     socket.to(channel).broadcast.emit('audio-message-from-server', channel, audioChunk);
   });
